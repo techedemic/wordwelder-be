b361fd5030c30e4b4195766c0f580f232564bde1
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2023-10-18 17:44:58 +0200

    Test query to see that prisma is working as expected

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const sentence = await prisma.sentence.create({
+    data: {
+      sentence: "Hello world",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
