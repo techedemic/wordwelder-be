03c6aa6ed99d54d0cd5698c7969cf7549e4ecad2
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2023-10-24 14:11:35 +0200

    Add a /words route to fetch all words from the database.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3100;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
+});
+
+app.get("/words", async (req: Request, res: Response) => {
+  const words = await prisma.word.findMany();
+  res.json(words);
 });
 
 app.listen(port, () => {
